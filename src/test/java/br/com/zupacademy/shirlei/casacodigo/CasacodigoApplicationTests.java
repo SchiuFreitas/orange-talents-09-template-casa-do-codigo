a2096ede0e4f2813dabdfaae0d5874dff925cc92
Shirlei <schiufreitas@gmai.com>
2021-10-04 09:45:19 -0300

    setup

---
+++ src/test/java/br/com/zupacademy/shirlei/casacodigo/CasacodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.shirlei.casacodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasacodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
