6b892115cd26b117283d4a39f8ec53f28d5a9347
Shirlei <schiufreitas@gmai.com>
2021-10-04 15:54:41 -0300

    3.1.4.2 Cadastro de novo autor

--- src/main/java/br/com/zupacademy/shirlei/casacodigo/autor/AutorRepository.java
+++ src/main/java/br/com/zupacademy/shirlei/casacodigo/autor/AutorRepository.java
@@ -3,7 +3,9 @@ package br.com.zupacademy.shirlei.casacodigo.autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, Long> { //JpaRepository fornece alguns métodos relacionados ao JPA, como liberar o contexto de persistência e excluir registros em um lote.
-
+    Optional<Autor> findByEmail(String email);
 }
